b6741e122c2b9bb9362cec2f71f81bbdfaaa7f9f
Dhana1026 <140480516+Dhana1026@users.noreply.github.com>
2023-10-06 16:24:52 +0530

    Update myjava.java

--- myjava.java
+++ myjava.java
@@ -2,7 +2,7 @@ class Simple{
 	
     public static void main(String args[]){  
 	
-     System.out.println("Hiiiiiikkkkkkkkkkkkkiieo Java");  
+     System.out.println("Hiiiiiikkkkkkkkkkkssssssssskkiieo Java");  
 	
     }  
 	
